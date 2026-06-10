c9dd0afc717d5a9e736624d5d21e09c401263ab4
dnyaneshwari2502 <dnyanurakshe123@gmail.com>
2026-06-10 17:47:05 -0600

    fixed workflow

--- src/generate_notes.py
+++ src/generate_notes.py
@@ -9,7 +9,9 @@ load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
 if not api_key:
-    raise ValueError("GEMINI_API_KEY is missing. Check your .env file.")
+    raise ValueError(
+        "GEMINI_API_KEY is missing. Check GitHub Secret or local .env file."
+    )
 
 client = genai.Client(api_key=api_key)
 
